377bdfd976945f2109f2a76220cb180c5024dfdc
Tiarnan Guinee <tiarnanguinee@Tiarnans-MacBook-Pro-2.local>
2024-05-23 21:05:18 +0100

    add apiURL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "https://cd48c9e8-e4fc-4728-ae3d-eeb050886734-dev.e1-eu-north-azure.choreoapis.dev/djangoreacttswebapp/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
